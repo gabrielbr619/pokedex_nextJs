5bcd289acc1605b8c0d678ed3ca2ebcf26e63c10
gabrielbr619 <gabrielbr619@hotmail.com>
2023-05-31 10:35:15 -0300

    fix eslint issues

--- components/card_1/Top_Container.js
+++ components/card_1/Top_Container.js
@@ -1,22 +1,22 @@
-import styles from '../../styles/card_1.module.css'
+import styles from "../../styles/card_1.module.css";
 
 export const Top_Container = () => {
-    return( 
-         <div className={styles.top_container}>
-             <div className={styles.top_corner_big_outer_circle}>
-                 <div className={styles.top_corner_big_inner_circle}/>         
-             </div>
-             <div className={styles.top_corner_small_circles_container}>
-                 <Small_Circle color={"#B90202"}/>
-                 <Small_Circle color={"#FDCB65"}/>
-                 <Small_Circle color={"#32CB64"}/>
-             </div>
-         </div>
-     )
- }
+	return( 
+		<div className={styles.top_container}>
+			<div className={styles.top_corner_big_outer_circle}>
+				<div className={styles.top_corner_big_inner_circle}/>         
+			</div>
+			<div className={styles.top_corner_small_circles_container}>
+				<Small_Circle color={"#B90202"}/>
+				<Small_Circle color={"#FDCB65"}/>
+				<Small_Circle color={"#32CB64"}/>
+			</div>
+		</div>
+	);
+};
  
 const Small_Circle = ({color}) => {
-    return(
-        <div style={{background: color}} className={styles.top_corner_small_circles}/>
-    )
-}
+	return(
+		<div style={{background: color}} className={styles.top_corner_small_circles}/>
+	);
+};
